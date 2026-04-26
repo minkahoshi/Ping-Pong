545a5df513ec754d450a0616fca426d2f5c04c0d
minkahoshi <minkagn@gmail.com>
2026-04-26 19:33:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,88 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+ 
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_s] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_DOWN] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+
+
+
+back = (200, 254, 254)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock.tick(FPS)
+FPS = 60
+img_ball = "котость.png"
+
+rocket1 = Player('racket.png', 30, 200, 4, 50, 150)
+rocket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('img_ball', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        if finish != True:
+            window.fill(back)
+            rocket1.update_l()
+            rocket2.update_r()
+            ball_rect_x += speed_x
+            ball_rect_y += speed_y
+          
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+          
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+          
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+
+        rocket1.reset()
+        rocket2.reset()
+        ball.reset()
+
+  display.update()
+    clock.tick(FPS)
